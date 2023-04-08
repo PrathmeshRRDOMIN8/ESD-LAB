5ed0fec2059b72c1ede43cfdf9fef0c10964339a
Prathmesh Sinha <77191932+PrathmeshRRDOMIN8@users.noreply.github.com>
2023-04-08 16:37:18 +0530

    LAB 8 Q 1 DONE

---
+++ 200953220_PrathmeshSinha/lab8q1.d
@@ -0,0 +1,8 @@
+lab8q1.o: Lab8Q1.c
+lab8q1.o: C:\Keil\ARM\Inc\NXP\LPC17xx\lpc17xx.h
+lab8q1.o: C:\Keil\ARM\CMSIS\Include\core_cm3.h
+lab8q1.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+lab8q1.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+lab8q1.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+lab8q1.o: C:\Keil\ARM\Inc\NXP\LPC17xx\system_LPC17xx.h
+lab8q1.o: C:\Keil\ARM\ARMCC\bin\..\include\stdlib.h
